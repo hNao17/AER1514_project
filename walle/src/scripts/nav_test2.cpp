ea803f72a43f1bc83dff133498df52a380bc35cc
hNao17 <hariso.khan@mail.utoronto.ca>
2017-02-26 15:26:26 -0500

    Update nav_test2.cpp
    -Modified waypoint list to include desired orientation
    -added 10[s] timeout to waitForResult

--- walle/src/scripts/nav_test2.cpp
+++ walle/src/scripts/nav_test2.cpp
@@ -4,17 +4,59 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 //global variables
-double path [][2] = { {22.0,13.5},
-	              {24.0,13.5},
-		      {26.0,13.5},
-                      {28.0,13.5},
-                      {30.0,13.5} };
+const double num_waypoints=45;
+const double dim_waypoint=3;
+double path [][3] = { {31.4,3.0,90.0},
+	              {31.4,4.0,90.0},
+		      {31.4,5.0,90.0},
+                      {31.4,6.0,90.0},
+                      {31.4,7.0,90.0},
+		      {31.4,8.0,90.0},
+		      {31.4,9.0,90.0},
+		      {31.4,10.0,90.0},
+		      {31.4,11.0,90.0},
+		      {31.4,12.0,90.0},
+		      {31.4,13.0,90.0},
+		      {31.4,14.0,90.0},
+		      {31.4,15.0,90.0},
+		      {31.4,16.0,90.0},
+		      {31.4,17.0,90.0},
+		      {31.4,18.0,90.0},
+		      {31.4,19.0,90.0},
+		      {31.4,20.0,90.0},
+   		      {31.4,21.0,90.0},
+		      {31.4,22.0,90.0},
+		      {31.4,23.0,90.0},
+		      {31.4,24.0,90.0},
+		      {31.4,25.0,90.0},
+		      {31.4,26.0,90.0},
+                      {31.4,27.0,90.0},
+		      {31.4,28.0,90.0},
+		      {30.4,28.0,180.0},
+		      {29.4,28.0,180.0},
+		      {28.4,28.0,180.0},
+		      {27.4,28.0,180.0},
+		      {26.4,28.0,180.0},
+		      {25.4,28.0,180.0},
+		      {24.4,28.0,180.0},
+		      {23.4,28.0,180.0},
+		      {22.4,28.0,180.0},
+		      {21.4,28.0,180.0},
+		      {20.4,28.0,180.0},
+		      {19.4,28.0,180.0},
+		      {18.4,28.0,180.0},
+		      {17.4,28.0,180.0},
+		      {16.4,28.0,180.0},
+		      {15.4,28.0,180.0},
+		      {14.4,28.0,180.0},
+		      {13.4,28.0,180.0} };
+
 ros::Subscriber sub;
 double x_current;
 double y_current;
 
 /** function declarations **/
-void moveToGoal(double xGoal, double yGoal);
+void moveToGoal(double xGoal, double yGoal, double yawGoal);
 void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL);
 
 int main(int argc, char** argv)
@@ -26,10 +68,11 @@ int main(int argc, char** argv)
 	sub = nh.subscribe("amcl_pose",1000,poseAMCLCallback);	
 	ros::spinOnce();
 
-	for (int i=0; i<5; i++)
+	for (int i=0; i<num_waypoints; i++)
 	{
 		//position command
-		moveToGoal(path[i][0], path[i][1]);
+		ROS_INFO_STREAM("Waypoint #"<<i);
+		moveToGoal(path[i][0], path[i][1], path[i][2]);
 
 		//determine position error
 		ros::spinOnce(); //check AMCL Callback
@@ -39,14 +82,15 @@ int main(int argc, char** argv)
 	}
 }
 
-void moveToGoal(double xGoal, double yGoal)
+void moveToGoal(double xGoal, double yGoal, double yawGoal)
 {
 
 	//define a client for to send goal requests to the move_base server through a SimpleActionClient
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
 
 	//wait for the action server to come up
-	while(!ac.waitForServer(ros::Duration(5.0))){
+	while(!ac.waitForServer(ros::Duration(5.0)))
+	{
 		ROS_INFO("Waiting for the move_base action server to come up");
 	}
 
@@ -61,15 +105,27 @@ void moveToGoal(double xGoal, double yGoal)
 	goal.target_pose.pose.position.x =  xGoal;
 	goal.target_pose.pose.position.y =  yGoal;
 	goal.target_pose.pose.position.z =  0.0;
-	goal.target_pose.pose.orientation.x = 0.0;
-	goal.target_pose.pose.orientation.y = 0.0;
-	goal.target_pose.pose.orientation.z = 0.0;
-	goal.target_pose.pose.orientation.w = 1.0;
+
+	//convert degrees to quaternion
+	if (yawGoal == 90.0)
+	{
+		goal.target_pose.pose.orientation.x = 0.0;
+		goal.target_pose.pose.orientation.y = 0.0;
+		goal.target_pose.pose.orientation.z = sqrt(2);
+		goal.target_pose.pose.orientation.w = sqrt(2);
+	}
+	else if(yawGoal == 180.0)
+	{
+		goal.target_pose.pose.orientation.x = 0.0;
+		goal.target_pose.pose.orientation.y = 0.0;
+		goal.target_pose.pose.orientation.z = 1.0;
+		goal.target_pose.pose.orientation.w = 0.009;
+	}
 
 	ROS_INFO("Sending goal location ...");
 	ac.sendGoal(goal);
 
-	ac.waitForResult();
+	ac.waitForResult(ros::Duration(10.0));
 
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
 	{
@@ -85,25 +141,16 @@ void moveToGoal(double xGoal, double yGoal)
 void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL)
 {
 
-	/*tf::Quaternion q (odom.pose.pose.orientation.x, 
-			  odom.pose.pose.orientation.y,
-			  odom.pose.pose.orientation.z,
-			  odom.pose.pose.orientation.w);
-
-	tf::Matrix3x3 m(q);
-	double roll, pitch, yaw;
-	m.getRPY(roll, pitch, yaw);*/
-
 	ROS_INFO_STREAM("Current turtlebot position: ("
                                <<msgAMCL.pose.pose.position.x <<","
 			       <<msgAMCL.pose.pose.position.y <<","
 			       <<msgAMCL.pose.pose.position.z <<")");
 
-	/*ROS_INFO_STREAM("Current turtlebot orientation: (0,0,"
-                               <<yaw*r2D<<")"); */
 
 	x_current = msgAMCL.pose.pose.position.x;
 	y_current = msgAMCL.pose.pose.position.y;
 }
+
+
 
 
