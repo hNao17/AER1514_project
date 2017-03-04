9a572be4255b55c3783f18b7219ef97eea946bac
hNao17 <hariso.khan@mail.utoronto.ca>
2017-03-04 17:37:06 -0500

    Update nav_test2.cpp
    -added readWaypoints() method, which inputs master waypoints from a textfile 
    - changed number of waypoints from 45 to 71

--- walle/src/scripts/nav_test2.cpp
+++ walle/src/scripts/nav_test2.cpp
@@ -2,60 +2,25 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-//global variables
-const double num_waypoints=45;
-const double dim_waypoint=3;
-double path [][3] = { {31.4,3.0,90.0},
-	              {31.4,4.0,90.0},
-		      {31.4,5.0,90.0},
-                      {31.4,6.0,90.0},
-                      {31.4,7.0,90.0},
-		      {31.4,8.0,90.0},
-		      {31.4,9.0,90.0},
-		      {31.4,10.0,90.0},
-		      {31.4,11.0,90.0},
-		      {31.4,12.0,90.0},
-		      {31.4,13.0,90.0},
-		      {31.4,14.0,90.0},
-		      {31.4,15.0,90.0},
-		      {31.4,16.0,90.0},
-		      {31.4,17.0,90.0},
-		      {31.4,18.0,90.0},
-		      {31.4,19.0,90.0},
-		      {31.4,20.0,90.0},
-   		      {31.4,21.0,90.0},
-		      {31.4,22.0,90.0},
-		      {31.4,23.0,90.0},
-		      {31.4,24.0,90.0},
-		      {31.4,25.0,90.0},
-		      {31.4,26.0,90.0},
-                      {31.4,27.0,90.0},
-		      {31.4,28.0,90.0},
-		      {30.4,28.0,180.0},
-		      {29.4,28.0,180.0},
-		      {28.4,28.0,180.0},
-		      {27.4,28.0,180.0},
-		      {26.4,28.0,180.0},
-		      {25.4,28.0,180.0},
-		      {24.4,28.0,180.0},
-		      {23.4,28.0,180.0},
-		      {22.4,28.0,180.0},
-		      {21.4,28.0,180.0},
-		      {20.4,28.0,180.0},
-		      {19.4,28.0,180.0},
-		      {18.4,28.0,180.0},
-		      {17.4,28.0,180.0},
-		      {16.4,28.0,180.0},
-		      {15.4,28.0,180.0},
-		      {14.4,28.0,180.0},
-		      {13.4,28.0,180.0} };
+/** global variables **/
+const int num_waypoints=71;
+const int dim_waypoint=3;
+double path [num_waypoints][dim_waypoint];
 
 ros::Subscriber sub;
 double x_current;
 double y_current;
 
+const double x_home=31.5;
+const double y_home=2.0;
+const double theta_home=90.0;
+
 /** function declarations **/
+void readWaypoints();
 void moveToGoal(double xGoal, double yGoal, double yawGoal);
 void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL);
 
@@ -64,22 +29,62 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "nav_test2");
 	ros::NodeHandle nh;
 
+    	//import waypoints from text file
+	readWaypoints();
+
 	//determine spawn location, check AMCL callback
-	sub = nh.subscribe("amcl_pose",1000,poseAMCLCallback);	
+	sub = nh.subscribe("amcl_pose",1000,poseAMCLCallback);
 	ros::spinOnce();
 
 	for (int i=0; i<num_waypoints; i++)
 	{
 		//position command
-		ROS_INFO_STREAM("Waypoint #"<<i);
+		ROS_INFO_STREAM("Waypoint #"<<i+1);
 		moveToGoal(path[i][0], path[i][1], path[i][2]);
 
 		//determine position error
 		ros::spinOnce(); //check AMCL Callback
 		ROS_INFO_STREAM("X Position Error: "<<path[i][0]-x_current);
-		ROS_INFO_STREAM("Y Position Error: "<<path[i][1]-y_current);		
-		ROS_INFO_STREAM("Next destination");		
+		ROS_INFO_STREAM("Y Position Error: "<<path[i][1]-y_current);
+		ROS_INFO_STREAM("Next destination");
 	}
+
+	//return home
+    	ROS_INFO_STREAM("Exploration Complete. Going home.");
+	moveToGoal(x_home,y_home,theta_home);
+}
+
+void readWaypoints()
+{
+    std::ifstream infile("/home/na052/AER1514/src/walle/src/scripts/masterWaypoints3.txt.csv");
+
+    double xPoint;
+    double yPoint;
+    double theta;
+    int waypointCounter=0;
+
+    if(infile.is_open())
+    {
+        while(infile>>xPoint>>yPoint>>theta)
+        {
+            path[waypointCounter][0]=xPoint;
+            path[waypointCounter][1]=yPoint;
+            path[waypointCounter][2]=theta;
+            waypointCounter++;
+        }
+
+        infile.close();
+
+        for(int i=0; i < num_waypoints; i++)
+        {
+            ROS_INFO_STREAM("X: "<<path[i][0]<<", Y: "<<path[i][1]<<", Theta: "<<path[i][2]);
+        }
+    }
+
+    else
+    {
+        ROS_INFO_STREAM("File is not open");
+    }
 }
 
 void moveToGoal(double xGoal, double yGoal, double yawGoal)
@@ -121,6 +126,13 @@ void moveToGoal(double xGoal, double yGoal, double yawGoal)
 		goal.target_pose.pose.orientation.z = 1.0;
 		goal.target_pose.pose.orientation.w = 0.009;
 	}
+    	else if(yawGoal == 135.0)
+	{
+		goal.target_pose.pose.orientation.x = 0.0;
+		goal.target_pose.pose.orientation.y = 0.0;
+		goal.target_pose.pose.orientation.z = 0.924;
+		goal.target_pose.pose.orientation.w = 0.383;
+	}
 
 	ROS_INFO("Sending goal location ...");
 	ac.sendGoal(goal);
@@ -142,7 +154,7 @@ void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL)
 {
 
 	ROS_INFO_STREAM("Current turtlebot position: ("
-                               <<msgAMCL.pose.pose.position.x <<","
+                    	       <<msgAMCL.pose.pose.position.x <<","
 			       <<msgAMCL.pose.pose.position.y <<","
 			       <<msgAMCL.pose.pose.position.z <<")");
 
@@ -150,7 +162,3 @@ void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL)
 	x_current = msgAMCL.pose.pose.position.x;
 	y_current = msgAMCL.pose.pose.position.y;
 }
-
-
-
-
