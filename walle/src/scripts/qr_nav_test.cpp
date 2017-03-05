43fcf6b9d804b1ccfeb385855303cbde33906b3c
hNao17 <hariso.khan@mail.utoronto.ca>
2017-03-05 10:18:04 -0500

    Update qr_nav_test.cpp
    -changed scanRotate loop condition; previously was time-based and now it is rotation based

--- walle/src/scripts/qr_nav_test.cpp
+++ walle/src/scripts/qr_nav_test.cpp
@@ -5,29 +5,32 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/Int8.h>
+#include <tf/tf.h>
 
 //global variables
-const double num_waypoints=6;
+const double num_waypoints=7;
 const double dim_waypoint=3;
-double path [][3] = { {31.4,3.0,90.0},
-	              {31.4,4.0,90.0},
-		      {31.4,5.0,90.0},
-                      {31.4,6.0,90.0},
-                      {31.4,7.0,90.0},
-		      {31.4,8.0,90.0}};
-		
+double path [][3] = { {31.5,3.0,90.0},
+                      {31.5,4.0,90.0},
+		              {31.5,5.0,90.0},
+		              {31.5,5.0,90.0},
+		              {31.5,6.0,90.0},
+		              {31.5,7.0,90.0},
+		              {31.5,8.0,90.0},};
 
 ros::Subscriber sub_amcl_pose;
 ros::Subscriber sub_visp_status;
 ros::Publisher pub_vel;
 double x_current;
 double y_current;
+double theta_current;
 
-double scan_time=8.0;
-double current_time=0.0;
-double initial_time=0.0;
+//double scan_time=8.0;
+//double current_time=0.0;
+//double initial_time=0.0;
 
-bool qrDetect = false;
+bool scanON = false;
+//bool qrDetect = false;
 int qr_detect_counter=0;
 
 
@@ -40,24 +43,24 @@ void scanRotate();
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "qr_nav_test1");
+	ros::init(argc, argv, "qr_nav_test");
 	ros::NodeHandle nh;
 
 	//determine spawn location, check AMCL callback
 	sub_amcl_pose = nh.subscribe("amcl_pose",1000,poseAMCLCallback);
 	sub_visp_status = nh.subscribe("visp_auto_tracker/status",1000,vispStatusCallback);
-	pub_vel = nh.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity",100);	
+	pub_vel = nh.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity",100);
 	//ros::spinOnce();
 
 	for (int i=0; i<num_waypoints; i++)
 	{
 		//position command
-		ROS_INFO_STREAM("Waypoint #"<<i);
+		ROS_INFO_STREAM("Waypoint #"<<i+1);
 		moveToGoal(path[i][0], path[i][1], path[i][2]);
 
 		ROS_INFO_STREAM("X Position Error: "<<path[i][0]-x_current);
-		ROS_INFO_STREAM("Y Position Error: "<<path[i][1]-y_current);		
-		ROS_INFO_STREAM("Next destination");		
+		ROS_INFO_STREAM("Y Position Error: "<<path[i][1]-y_current);
+		ROS_INFO_STREAM("Next destination");
 	}
 }
 
@@ -80,13 +83,19 @@ void moveToGoal(double xGoal, double yGoal, double yawGoal)
 	goal.target_pose.header.stamp = ros::Time::now();
 
 	/* moving towards the goal*/
-
 	goal.target_pose.pose.position.x =  xGoal;
 	goal.target_pose.pose.position.y =  yGoal;
 	goal.target_pose.pose.position.z =  0.0;
 
 	//convert degrees to quaternion
-	if (yawGoal == 90.0)
+	if(yawGoal == 0.0)
+    {
+        goal.target_pose.pose.orientation.x = 0.0;
+		goal.target_pose.pose.orientation.y = 0.0;
+		goal.target_pose.pose.orientation.z = 0.0;
+		goal.target_pose.pose.orientation.w = 1.0;
+    }
+	else if (yawGoal == 90.0)
 	{
 		goal.target_pose.pose.orientation.x = 0.0;
 		goal.target_pose.pose.orientation.y = 0.0;
@@ -100,17 +109,31 @@ void moveToGoal(double xGoal, double yGoal, double yawGoal)
 		goal.target_pose.pose.orientation.z = 1.0;
 		goal.target_pose.pose.orientation.w = 0.009;
 	}
+    else if(yawGoal == 270.0)
+	{
+		goal.target_pose.pose.orientation.x = 0.0;
+		goal.target_pose.pose.orientation.y = 0.0;
+		goal.target_pose.pose.orientation.z = sqrt(2);
+		goal.target_pose.pose.orientation.w = -sqrt(2);
+	}
+    else if(yawGoal == 359.0)
+	{
+		goal.target_pose.pose.orientation.x = 0.0;
+		goal.target_pose.pose.orientation.y = 0.0;
+		goal.target_pose.pose.orientation.z = 0.0087;
+		goal.target_pose.pose.orientation.w = -1.0;
+	}
 
 	ROS_INFO("Sending goal location ...");
 	ac.sendGoal(goal);
 
-	ac.waitForResult(ros::Duration(10.0));
+	ac.waitForResult(ros::Duration(30.0));
 
 	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
 	{
 		ROS_INFO("You have reached the destination");
 		scanRotate();
-		qr_detect_counter=0;
+		//qr_detect_counter=0;
 	}
 	else
 	{
@@ -122,22 +145,23 @@ void moveToGoal(double xGoal, double yGoal, double yawGoal)
 void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL)
 {
 
-	/*tf::Quaternion q (odom.pose.pose.orientation.x, 
-			  odom.pose.pose.orientation.y,
-			  odom.pose.pose.orientation.z,
-			  odom.pose.pose.orientation.w);
+	tf::Quaternion q (msgAMCL.pose.pose.orientation.x,
+			 msgAMCL.pose.pose.orientation.y,
+             msgAMCL.pose.pose.orientation.z,
+             msgAMCL.pose.pose.orientation.w);
 
 	tf::Matrix3x3 m(q);
 	double roll, pitch, yaw;
-	m.getRPY(roll, pitch, yaw);*/
+	m.getRPY(roll, pitch, yaw);
 
 	ROS_INFO_STREAM("Current turtlebot position: ("
-                               <<msgAMCL.pose.pose.position.x <<","
+                   <<msgAMCL.pose.pose.position.x <<","
 			       <<msgAMCL.pose.pose.position.y <<","
 			       <<msgAMCL.pose.pose.position.z <<")");
 
-	/*ROS_INFO_STREAM("Current turtlebot orientation: (0,0,"
-                               <<yaw*r2D<<")"); */
+    theta_current = yaw;
+
+	ROS_INFO_STREAM("Current turtlebot orientation: "<<theta_current<<" [radians]");
 
 	x_current = msgAMCL.pose.pose.position.x;
 	y_current = msgAMCL.pose.pose.position.y;
@@ -146,50 +170,76 @@ void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped& msgAMCL)
 void scanRotate()
 {
 	geometry_msgs::Twist vel_msg;
-	vel_msg.linear.x =0;	
+	vel_msg.linear.x =0;
 	vel_msg.angular.z=M_PI/4;
 
-	initial_time = ros::Time::now().toSec();
+	double theta_previous;
+	double theta_complete=0;
+	double del_theta;
+
+	//initial_time = ros::Time::now().toSec();
 	ROS_INFO_STREAM("Initating QR detect");
 	ros::Rate loop_rate(100);
-	while ((current_time-initial_time) < scan_time && qrDetect == false)
+	ros::spinOnce();
+
+	theta_previous = theta_current;
+	//while ((current_time-initial_time) < scan_time && qrDetect == false)
+    while(theta_complete < 2*M_PI)
 	{
-		current_time = ros::Time::now().toSec();
+	    scanON = true;
+		//current_time = ros::Time::now().toSec();
 		pub_vel.publish(vel_msg);
-		//check if visp has detected qr code
 		ros::spinOnce();
-						
+
+        //wait for qr code to be processed
+        if(qr_detect_counter >=5)
+        {
+            vel_msg.angular.z=0;
+            pub_vel.publish(vel_msg);
+            ros::Duration(5.0).sleep();
+
+            //start moving
+            vel_msg.angular.z=M_PI/4;
+            pub_vel.publish(vel_msg);
+
+            qr_detect_counter = 0;
+        }
+
+		del_theta = theta_current - theta_previous;
+
+		if(abs(del_theta)>M_PI)
+            del_theta+= 2*M_PI;
+
+        theta_complete +=  del_theta;
+        theta_previous = theta_current;
+
 	}
-	
+
 	//QR code found; stop rotating
-	vel_msg.linear.x =0;	
+	vel_msg.linear.x =0;
 	vel_msg.angular.z=0;
 	pub_vel.publish(vel_msg);
 
-	//ros::spinOnce();
-	
+    scanON = false;
 	ROS_INFO_STREAM("Finished QR detect");
-	ros::Duration(5.0).sleep();
-	qrDetect=false;
 }
 
 void vispStatusCallback(const std_msgs::Int8& msgVispStatus)
 {
-	//ROS_INFO_STREAM("visp status: "<<msgVispStatus.data);
+	//ROS_INFO_STREAM("Visp Callback Started");
 	if (msgVispStatus.data == 3 || msgVispStatus.data == 4)
 	{
-		ROS_INFO_STREAM("Found QR Code");
-		ROS_INFO_STREAM("qr detect counter: "<<qr_detect_counter);
-		qr_detect_counter++; //sample visp_auto_tracker
-	
-		if(qr_detect_counter > 5)
-		{
-			qrDetect=true; //only set true if multiple readings are found
-			qr_detect_counter=0;
-		}
+	    //ROS_INFO_STREAM("visp status: "<<msgVispStatus.data);
+	    if(scanON==true)
+        {
+            ROS_INFO_STREAM("Found QR Code");
+            ROS_INFO_STREAM("qr detect counter: "<<qr_detect_counter);
+            qr_detect_counter++; //sample visp_auto_tracker
+        }
+
 	}
-	
-	
+
+
 }
 
 /*void vispPoseCallback(const geometry_msgs::PoseStamped& msgVispPose)
