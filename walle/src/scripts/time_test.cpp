e2850038918cc27f38d2a250671d408c87433823
hNao17 <hNao17@github.com>
2017-03-12 19:02:10 -0400

    added msg folder, updated cmakelists, timer_test.cpp

--- walle/src/scripts/time_test.cpp
+++ walle/src/scripts/time_test.cpp
@@ -1,20 +1,26 @@
 #include <ros/ros.h>
+#include "walle/exploreState.h"
 //#include<rosgraph_msgs/Clock.h>
 //#include <wall_timer.h>
 
 bool time_exceeded = false;
 double allowable_time = 50.0;
 //initialise time_exceeded message publisher
+ros::Publisher pub;
 
 int main(int argc, char** argv)
 {
     ros::init(argc,argv,"time_test");
     ros::NodeHandle nh;
 
+    pub=nh.advertise<walle::exploreState>("/publishMsg",1000);
+
     double start_time = ros::Time::now().toSec();
     double current_time;
 
-    //ros::Rate rate(0.1);
+    walle::exploreState msg;
+
+    ros::Rate rate(10);
     while(time_exceeded==false)
     {
         current_time=ros::Time::now().toSec();
@@ -23,7 +29,8 @@ int main(int argc, char** argv)
             time_exceeded = true;
 
         //publish message
-
+        msg.return_home=time_exceeded;
+        pub.publish(msg);
         ROS_INFO_STREAM("Elasped Time = "<<current_time-start_time<<"[s]");
     }
 
